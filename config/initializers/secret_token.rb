87c8664f9fe38f6928716954ab09ab3bc9256386
Azat Akhmetov <azat.a.akhmetov@gmail.com>
2012-11-08 23:29:17 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoinsColl::Application.config.secret_token = 'daa6fd5988997e98c85685a0cbf83a7b9c267fa9087a186fbf22968a085ec7cbb2f2f2fb0837461dfef7b1d923a8e6ce2c9b4b13091ab483c7e71ac5cb154807'
